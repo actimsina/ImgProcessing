322f3ce8ce164dca444d3523bd815745f366778a
Achyut Timsina <actimsina@gmail.com>
2017-08-29 12:58:28 +0545

    new database schema

--- ddoocp.sql
+++ ddoocp.sql
@@ -2,16 +2,11 @@ create table users(
 	id int primary key auto_increment,
 	name varchar(50),
 	pwd varchar(100)
+	image_path varchar(30),
+	thumbnail_path varchar(30)
 );
 
-create table images(
-	id int primary key auto_increment,
-	image blob,
-	thumbnails blob,
-	user_id int,
-	foreign key(user_id) references users(id)
-);
-
+-- 32 x 32
 -- 640 x 480
 -- 960 x 720
 -- 800 x 600
